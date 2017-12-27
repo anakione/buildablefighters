c139edf5a5fb59139efe802e23022f31dc2590af
unknown <andy1atkin@gmail.com>
2017-12-27 10:56:47 -0800

    updated to 1.9.1

--- common/defines/bfighters_defines.lua
+++
@@ -1,4 +0,0 @@
-NDefines.NGameplay.BASE_FLEET_SIZE = 40
-NDefines.NGameplay.NAVY_SIZE_BASE = 40
-NDefines.NGameplay.NAVY_SIZE_POP_MULT = 1
-NDefines.NGameplay.NAVY_SIZE_SPACEPORT_LEVEL = { 8, 16, 20, 36, 48, 60 }
